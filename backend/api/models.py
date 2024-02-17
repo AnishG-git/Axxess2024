df8de56ebd7a298f2ca2736b4a402332c94b1c53
Anish <anish6973@gmail.com>
2024-02-17 15:43:43 -0600

    added register, login, logout functionality

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,9 +6,9 @@ class AppUser(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=10)
-    age = models.PositiveIntegerField(null=True, blank=True)
+    dob = models.DateField(null=True, blank=True)
     pack_history = models.IntegerField(null=True, blank=True)
-    gender = models.CharField(max_length=100, null=True, blank=True)
+    sex = models.CharField(max_length=100, null=True, blank=True)
 
 
 # Create your models here.
