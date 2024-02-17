01d7da35a0521e00ed9315993908fce9e0c88e3b
TusharW4ni <reachtusharwani@gmail.com>
2024-02-17 15:47:45 -0600

    made login and signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+
+function Signup() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState(new Date());
+
+  const handleInputChange = event => {
+    let formattedInput = event.target.value.split('-').join('');
+    if (formattedInput.length > 3 && formattedInput.length <= 6) formattedInput = formattedInput.slice(0, 3) + '-' + formattedInput.slice(3);
+    else if (formattedInput.length > 6) formattedInput = formattedInput.slice(0, 3) + '-' + formattedInput.slice(3, 6) + '-' + formattedInput.slice(6);
+    setPhoneNumber(formattedInput);
+  };
+
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-3/5 bg-white p-6 rounded shadow flex flex-col justify-evenly">
+          <h1 className="text-3xl font-bold text-center">Sign Up</h1>
+          <form className="mt-6 flex flex-wrap -mx-3">
+            <div className="w-full md:w-1/2 px-3 mb-5">
+              <div className="mb-5">
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-600">
+                  Name
+                </label>
+                <input type="text" id="name" name="name" placeholder="Name" className="w-full p-4 border rounded-md outline-none hover:border-blue-500" />
+              </div>
+              <div className="mb-5">
+                <label htmlFor="phonenumber" className="block mb-2 text-sm font-medium text-gray-600">
+                  Phone Number
+                </label>
+                <input
+                  type="tel"
+                  id="phonenumber"
+                  name="phonenumber"
+                  placeholder="Phone Number"
+                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                  value={phoneNumber}
+                  onChange={handleInputChange}
+                  className="w-full p-4 border rounded-md outline-none hover:border-blue-500"
+                />
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="dateofbirth" className="block mb-2 text-sm font-medium text-gray-600">
+                  Date of Birth
+                </label>
+                <DatePicker onChange={setDateOfBirth} value={dateOfBirth} className="w-full p-4 border rounded-md outline-none hover:border-blue-500" />
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 px-3 mb-5">
+              <div className="mb-5">
+                <label htmlFor="sex" className="block mb-2 text-sm font-medium text-gray-600">
+                  Sex
+                </label>
+                <select id="sex" name="sex" className="w-full p-4 border rounded-md outline-none hover:border-blue-500">
+                  <option value="">Select...</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+
+              <div className="mb-5">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-600">
+                  Email
+                </label>
+                <input type="email" id="email" name="email" placeholder="Email" className="w-full p-4 border rounded-md outline-none hover:border-blue-500" />
+              </div>
+              <div className="mb-5">
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-600">
+                  Password
+                </label>
+                <input type="password" id="password" name="password" placeholder="Password" className="w-full p-4 border rounded-md outline-none hover:border-blue-500" />
+              </div>
+            </div>
+          </form>
+
+          <button className="w-full p-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">Enter</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
