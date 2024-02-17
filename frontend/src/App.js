a7dc1d107e8b95dffd11ad080feb9c4a8600bbfd
nmaraboyina <112922058+nmaraboyina@users.noreply.github.com>
2024-02-17 17:51:29 -0600

    dashboard commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
