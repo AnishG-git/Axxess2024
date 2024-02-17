975f7cd46a2f164921a2e884810926e435048a80
TusharW4ni <reachtusharwani@gmail.com>
2024-02-17 13:59:41 -0600

    routes setup

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -3,12 +3,7 @@ import React from 'react';
 function Homepage() {
   return (
     <div>
-        <div className="fixed ali">
-
-        </div>
-        <div>
-
-        </div>
+      <h1>Homepage</h1>
     </div>
   );
 }
