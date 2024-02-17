c0b77e5094d5466561d63ed9336d5c44dd0fbc85
nmaraboyina <112922058+nmaraboyina@users.noreply.github.com>
2024-02-17 14:00:15 -0600

    initial commit

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Homepage() {
   return (
     <div>
-        <div className="fixed ali">
+        <div className="fixed bottom-0 left-0 w-full z-0">
 
         </div>
-        <div>
+        <div className="flex flex-wrap w-full z-10">
 
         </div>
     </div>
